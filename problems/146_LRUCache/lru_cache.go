80432d8179129272c0328bd4dd41d6634a3dca71
Vasiliy Tsybenko <vasayxtx@gmail.com>
2019-02-24 22:21:56 +0300

    Solve problem #146

---
+++ problems/146_LRUCache/lru_cache.go
@@ -0,0 +1,60 @@
+package lrucache
+
+import "container/list"
+
+type Item struct {
+	Key   int
+	Value int
+}
+
+type LRUCache struct {
+	Capacity  int
+	itemsMap  map[int]*list.Element
+	itemsList *list.List
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		Capacity:  capacity,
+		itemsMap:  make(map[int]*list.Element),
+		itemsList: list.New(),
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	el, ok := this.itemsMap[key]
+	if !ok {
+		return -1
+	}
+	this.itemsList.MoveToFront(el)
+	return el.Value.(*Item).Value
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	el, ok := this.itemsMap[key]
+	if ok {
+		el.Value.(*Item).Value = value
+		this.itemsList.MoveToFront(el)
+		return
+	}
+
+	el = this.itemsList.PushFront(&Item{key, value})
+	this.itemsMap[key] = el
+
+	if this.itemsList.Len() > this.Capacity {
+		el = this.itemsList.Back()
+		this.itemsList.Remove(el)
+		delete(this.itemsMap, el.Value.(*Item).Key)
+	}
+}
+
+func (this *LRUCache) Len() int {
+	return this.itemsList.Len()
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
---
+++ problems/146_LRUCache/lru_cache_test.go
@@ -0,0 +1,39 @@
+package lrucache
+
+import "testing"
+
+func assertVal(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("expected %d, actual %d", expected, actual)
+	}
+}
+
+func TestLRUCache(t *testing.T) {
+	cache := Constructor(3)
+	assertVal(t, 0, cache.Len())
+	assertVal(t, -1, cache.Get(1))
+
+	cache.Put(1, 10)
+	assertVal(t, 1, cache.Len())
+	assertVal(t, 10, cache.Get(1))
+
+	cache.Put(2, 20)
+	cache.Put(3, 30)
+	assertVal(t, 3, cache.Len())
+	assertVal(t, 10, cache.Get(1))
+	assertVal(t, 20, cache.Get(2))
+	assertVal(t, 30, cache.Get(3))
+
+	cache.Put(3, 40)
+	assertVal(t, 40, cache.Get(3))
+	cache.Put(3, 30)
+	assertVal(t, 30, cache.Get(3))
+	assertVal(t, 3, cache.Len())
+
+	cache.Put(4, 40)
+	assertVal(t, 3, cache.Len())
+	assertVal(t, -1, cache.Get(1))
+	assertVal(t, 20, cache.Get(2))
+	assertVal(t, 30, cache.Get(3))
+	assertVal(t, 40, cache.Get(4))
+}
